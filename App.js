656d0940ffe1774c3dac687b7b78d7622b377889
debora958 <109834177+debora958@users.noreply.github.com>
2022-11-27 19:33:14 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Route,Link,Routes} from 'react-router-dom'
+import Pg1 from './componentes/Pagina1'
+import Pg2 from './componentes/Pagina2'
+import Pg3 from './componentes/Pagina3'
+
+export default function App(){
+  return (
+    <>
+      <header>
+        <nav>
+        <Link to='/pag1'>Página 1</Link>
+        <Link to='/pag2'>Página 2</Link>
+        <Link to='/pag3'>Página 3</Link>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route exact path="/pag1" element={<Pg1/>}/>
+          <Route exact path="/pag2" element={<Pg2/>}/>
+          <Route exact path="/pag3" element={<Pg3/>}/>  
+        </Routes>
+      </main>
+    </>
+  )
+}
---
+++ Pagina2.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Pagina2(){
+    return(
+        <div>
+            <h1>Página 2</h1>
+            <h3>Desenvolvedor FrontEdnd</h3>
+        </div>
+    );
+    
+}
